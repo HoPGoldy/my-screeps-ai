be5a7ce644519f824b16e270cf18537eb3f463cb
hopgoldy <hpgless@outlook.com>
2019-10-08 18:45:45 +0800

    fix: 完成角色分组

--- src/role.ts
+++ src/role.ts
@@ -1,31 +1,9 @@
-import harvester from './role.harvester'
-import upgrader from './role.upgrader'
-import builder from './role.builder'
-import repairer from './role.repairer'
-import claimer from './role.claimer'
-import soldier from './role.soldier'
-import transfer from './role.transfer'
-import remoteHarvester from './role.remoteHarvester'
-import remoteBuilder from './role.remoteBuilder'
-import remoteUpgrader from './role.remoteUpgrader'
-import towerTransfer from './role.towerTransfer'
-
 import baseRoles from './roles.base'
 import remoteRoles from './roles.remote'
+import warRoles from './roles.war'
 
 export default {
     ...baseRoles,
-    // harvester,
-    // upgrader,
-    // builder,
-    // repairer,
-    // towerTransfer,
-
-    soldier,
-
-    ...remoteRoles,
-    // claimer,
-    // remoteBuilder,
-    // remoteUpgrader,
-    // remoteHarvester
+    ...warRoles,
+    ...remoteRoles
 }
---
+++ src/roles.war.ts
@@ -0,0 +1,50 @@
+/**
+ * 战斗角色组
+ * 本角色组包括了对外战斗和房间防御所需要的角色
+ */
+export default {
+    /**
+     * 士兵配置生成器
+     * source: 无
+     * target: 向旗帜发起进攻
+     * 
+     * @param spawnName 出生点名称
+     * @param bodys 身体部件 (可选)
+     */
+    soldier: (spawnName: string, bodys: BodyPartConstant[] = [ ATTACK, MOVE, MOVE ]): ICreepConfig => ({
+        target: creep => creep.attackFlag(),
+        spawn: spawnName,
+        bodys
+    }),
+
+    /**
+     * 医生配置生成器
+     * source: 无
+     * target: 治疗给定的 creep
+     * 
+     * @param spawnName 出生点名称
+     * @param creepsName 要治疗的 creep 名称数组
+     * @param bodys 身体部件 (可选)
+     */
+    doctor: (spawnName: string, creepsName: string[], bodys: BodyPartConstant[] = [ HEAL, MOVE, MOVE ]): ICreepConfig => ({
+        target: creep => creep.healTo(creepsName.map(name => Game.creeps[name])),
+        spawn: spawnName,
+        bodys
+    }),
+
+    /**
+     * 防御者配置生成器
+     * source: 到 "房间名 StandBy" 旗帜下待命
+     * target: 攻击出现的敌人
+     * 
+     * @param spawnName 出生点名称
+     * @param bodys 身体部件 (可选)
+     */
+    defender: (spawnName: string, bodys: BodyPartConstant[] = [ ATTACK, MOVE, MOVE ]): ICreepConfig => ({
+        source: creep => creep.standBy(),
+        target: creep => creep.defense(),
+        switch: creep => creep.checkEnemy(),
+        spawn: spawnName,
+        bodys
+    })
+}
