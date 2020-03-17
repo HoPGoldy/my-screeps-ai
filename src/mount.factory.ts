e7042b05f43f1542c21ac7ad45a9256e1bc4400f
huang puguang <puguang_huang@fpi-inc.com>
2020-03-17 15:31:29 +0800

    fix: 优化 factory 继续生产检查

--- src/mount.factory.ts
+++ src/mount.factory.ts
@@ -36,7 +36,6 @@ export default class FactoryExtension extends StructureFactory {
                 this.getResource()
             break
             case FACTORY_STATE.WORKING:
-                if (Game.time % 3) return
                 this.working()
             break
             case FACTORY_STATE.PUT_RESOURCE:
@@ -157,7 +156,16 @@ export default class FactoryExtension extends StructureFactory {
         // console.log('执行合成!')
         
         // 没有冷却好就直接跳过
-        if (this.cooldown !== 0) return
+        if (this.cooldown !== 0) {
+            if (this.room.memory.factory.produceCheck) {
+                console.log(`[${this.room.name} factory] 执行检查`)
+                // 发现材料不足了就进入下个阶段
+                if (!this.canContinueProduce()) this.room.memory.factory.state = FACTORY_STATE.PUT_RESOURCE
+                // 移除标志位，每个冷却阶段只坚持一次材料是否充足就够了
+                delete this.room.memory.factory.produceCheck
+            }
+            return
+        }
 
         const task = this.getCurrentTask()
         // 一般到这一步是不会产生没有任务的问题
@@ -168,11 +176,35 @@ export default class FactoryExtension extends StructureFactory {
 
         const actionResult = this.produce(task.target)
         
-        // 底物不足了说明合成完毕
-        // 这里会导致要等到冷却完成之后才会进入下个阶段，是个优化点
-        if (actionResult === ERR_NOT_ENOUGH_RESOURCES) this.room.memory.factory.state = FACTORY_STATE.PUT_RESOURCE
+        // 成功生产了就将举起检查标志位，等待下个 tick 检查底物数量
+        if (actionResult === OK) this.room.memory.factory.produceCheck = true
+        // 这里只是个兜底，一般情况下在上面的 this.canContinueProduce() 判断后就已经确定了是否要进入下个阶段
+        else if (actionResult === ERR_NOT_ENOUGH_RESOURCES) this.room.memory.factory.state = FACTORY_STATE.PUT_RESOURCE
         else if (actionResult === ERR_INVALID_TARGET || actionResult === ERR_BUSY) this.requirePower()
-        else if (actionResult !== OK) console.log(`[${this.room.name} factory] working 阶段出现异常，错误码: ${actionResult}`)
+        else console.log(`[${this.room.name} factory] working 阶段出现异常，错误码: ${actionResult}`)
+    }
+
+    /**
+     * 检查当前 factory 中的底物是否足够再次生产
+     * 
+     * @returns true 表示可以继续生产，false 表示无法继续生产
+     */
+    private canContinueProduce(): boolean {
+        // 获取当前任务
+        const task = this.getCurrentTask()
+        if (!task) {
+            this.room.memory.factory.state = FACTORY_STATE.PREPARE
+            return false
+        }
+
+        // 遍历任务目标所需的材料，如果有一项材料不足了，就说明无法继续生产
+        const subRes = COMMODITIES[task.target].components
+        for (const res in subRes) {
+            if (this.store[res] < subRes[res]) return false
+        }
+
+        // 所有材料都充足，可以继续生产
+        return true
     }
 
     /**
