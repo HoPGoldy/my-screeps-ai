bb367c55a341749ec976a2ed10d6995d26ed8919
huang puguang <puguang_huang@fpi-inc.com>
2020-06-29 11:13:17 +0800

    feat: 日志支持房间跳转

--- src/utils.ts
+++ src/utils.ts
@@ -48,18 +48,21 @@ export function doing(hashMap: object, showCpu: string = ''): void {
  * @param colorName 要添加的颜色常量字符串
  * @param bolder 是否加粗
  */
-export function colorful(content: string, colorName: Colors, bolder: boolean = false): string {
+export function colorful(content: string, colorName: Colors = null, bolder: boolean = false): string {
+    const colorStyle = colorName ? `color: ${colors[colorName]};` : ''
     const bolderStyle = bolder ? 'font-weight: bolder;' : ''
-    return `<text style="color: ${colors[colorName]}; ${bolderStyle}">${content}</text>`
+
+    return `<text style="${[ colorStyle, bolderStyle ].join(' ')}">${content}</text>`
 }
 
 /**
  * 生成控制台链接
  * @param content 要显示的内容
  * @param url 要跳转到的 url
+ * @param newTab 是否在新标签页打开
  */
-export function createLink(content: string, url: string): string {
-    return `<a href="${url}" target="_blank">${content}</a>`
+export function createLink(content: string, url: string, newTab: boolean = true): string {
+    return `<a href="${url}" target="${newTab ? '_blank' : '_self'}">${content}</a>`
 }
 
 /**
@@ -287,11 +290,11 @@ export function getCreepName(role: CreepRoleConstant, roomName: string, index?: 
  * @param color 日志前缀颜色
  * @param notify 是否发送邮件
  */
-export function log(content: string, prefixes: string[] = [], color: Colors = undefined, notify: boolean = false): OK {
+export function log(content: string, prefixes: string[] = [], color: Colors = null, notify: boolean = false): OK {
     // 有前缀就组装在一起
-    let prefix = prefixes.length > 0 ? `[${prefixes.join(' ')}] ` : ''
+    let prefix = prefixes.length > 0 ? `【${prefixes.join(' ')}】` : ''
     // 指定了颜色
-    if (color) prefix = colorful(prefix, color)
+    prefix = colorful(prefix, color, true)
 
     const logContent = `${prefix}${content}`
     console.log(logContent)
