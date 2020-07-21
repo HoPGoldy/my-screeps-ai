6d0acad6bedadfdd72b283c1f949c1678ea5af2a
hopgoldy <hpgless@outlook.com>
2020-07-21 20:14:08 +0800

    feat: 升级 Room.help

--- src/help.ts
+++ src/help.ts
@@ -1,90 +1,151 @@
 import { colorful } from "./utils"
 
 /**
- * 给函数的显示添加一点小细节
- * 只会用在各种 help 方法中
+ * 创建帮助信息
+ * 给帮助的显示添加一点小细节
  * 
- * @param functionInfo 函数的信息
+ * @param modules 模块的描述
  */
-export function createHelp(module: ModuleDescribe): string {
-    const functionList = module.api.map(createApiHelp)
+export function createHelp(...modules: ModuleDescribe[]): string {
+    return moduleStyle() + apiStyle() + `<div class="module-help">${modules.map(createModule).join('')}</div>`
+}
+
+const createModule = function(module: ModuleDescribe): string {
+    const functionList = module.api.map(createApiHelp).join('')
     
-    return apiStyle + functionList.join('\n')
+    const html = `<div class="module-container">
+        <div class="module-info">
+            <span class="module-title">${colorful(module.name, 'yellow')}</span>
+            <span class="module-describe">${colorful(module.describe, 'green')}</span>
+        </div>
+        <div class="module-api-list">${functionList}</div>
+    </div>`
+
+    return html.replace(/\n/g, '')
 }
 
 const createApiHelp = function(func: FunctionDescribe): string {
-    // 标题
-    const title = colorful(func.title, 'green')
+    const contents: string[] = []
+    // 介绍
+    if (func.describe) contents.push(colorful(func.describe, 'green'))
+
     // 参数介绍
-    const param = func.params ? 
-        func.params.map(param => `  - ${colorful(param.name, 'blue')}: ${colorful(param.desc, 'green')}`).join('\n') : ''
+    if (func.params) contents.push(func.params.map(param => {
+        return `  - ${colorful(param.name, 'blue')}: ${colorful(param.desc, 'green')}`
+    }).map(s => `<div class="api-content-line">${s}</div>`).join(''))
+
     // 函数示例中的参数
-    const paramInFunc = func.params ? 
-        func.params.map(param => colorful(param.name, 'blue')).join(', ') : ''
+    const paramInFunc = func.params ? func.params.map(param => colorful(param.name, 'blue')).join(', ') : ''
+
     // 函数示例
-    const functionName = `${colorful(func.functionName, 'yellow')}(${paramInFunc})`
+    contents.push(`${colorful(func.functionName, 'yellow')}(${paramInFunc})`)
 
+    const content = contents.map(s => `<div class="api-content-line">${s}</div>`).join('')
     const checkboxId = `${func.functionName}${Game.time}`
-
+    
     // return func.params ? `${title}\n${param}\n${functionName}\n` : `${title}\n${functionName}\n`
 
-    return `
-    <div class="container">
-    <label for="${checkboxId}">${func.title} ${functionName}</label>
-    <input id="${checkboxId}" type="checkbox" />
-    <div class="content">
-        <div>介绍</div>
-        <div>参数特别多多多多多多多多多多</div>
-        <div>返回值</div>
-    </div>
+    const result = `
+    <div class="api-container">
+        <label for="${checkboxId}">${func.title} ${colorful(func.functionName, 'yellow', true)}</label>
+        <input id="${checkboxId}" type="checkbox" />
+        <div class="api-content">${content}</div>
     </div>
     `
+
+    return result.replace(/\n/g, '')
 }
 
-const apiStyle = `
-<style>
-label {
-    transition: all 0.1s;
-    min-width: 300px;
-}
-.container {
-    margin: 10px;
-    width: 250px;
-    background-color:#f2f2f2;
-    overflow: hidden;
+const moduleStyle = function() {
+    const style = `
+    <style>
+    .module-help {
+        display: flex;
+        flex-flow: column nowrap;
+    }
+    .module-container {
+        padding: 0px 10px 10px 10px;
+        display: flex;
+        flex-flow: column nowrap;
+    }
+    .module-info {
+        margin: 5px;
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: baseline;
+    }
+    .module-title {
+        font-size: 19px;
+        font-weight: bolder;
+        margin-left: -15px;
+    }
+    .module-api-list {
+        display: flex;
+        flex-flow: row wrap;
+    }
+    </style>`
+
+    return style.replace(/\n/g, '')
 }
 
-/* 隐藏input */
-input {
-    display: none;
-}
+const apiStyle = function() {
+    const style = `
+    <style>
+    .api-content-line {
+        width: max-content;
+        padding-right: 15px;
+    }
+    .api-container {
+        margin: 5px;
+        width: 250px;
+        background-color: #2b2b2b;
+        overflow: hidden;
+        display: flex;
+        flex-flow: column;
+    }
 
-label {
-    cursor: pointer;
-    display: block;
-    margin: 0 0 0 -50px;
-    padding: 15px 10px 15px 70px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-}
+    .api-container label {
+        transition: all 0.1s;
+        min-width: 300px;
+        
+    }
+    
+    /* 隐藏input */
+    .api-container input {
+        display: none;
+    }
+    
+    .api-container label {
+        cursor: pointer;
+        display: block;
+        padding: 10px;
+        background-color: #3b3b3b;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    
+    .api-container label:hover, label:focus {
+        background-color: #525252;
+    }
+    
+    /* 清除所有展开的子菜单的 display */
+    .api-container input + .api-content {
+        overflow: hidden;
+        transition: all 0.1s;
+        width: auto;
+        max-height: 0px;
+        padding: 0px 10px;
+    }
+    
+    /* 当 input 被选中时，给所有展开的子菜单设置样式 */
+    .api-container input:checked + .api-content {
+        max-height: 200px;
+        padding: 10px;
+        background-color: #1c1c1c;
+        overflow-x: auto;
+    }
+    </style>`
 
-.container label:hover, label:focus {
-    background-color:#a7a7a7;
-    color:#fff;
+    return style.replace(/\n/g, '')
 }
-
-/* 清除所有展开的子菜单的 display */
-.container input + .content {
-    transition: all 0.1s;
-    width: auto;
-    max-height: 0px;
-    padding: 0px 10px
-}
-
-/* 当 input 被选中时，给所有展开的子菜单设置样式 */
-.container input:checked + .content {
-    max-height: 200px;
-    padding: 10px
-}
-</style>`
