95e9d213e47a614cccfc12b3d005857747292bc7
huang puguang <puguang_huang@fpi-inc.com>
2020-07-22 10:28:02 +0800

    fix: seeres 添加总量显示

--- src/mount.global.ts
+++ src/mount.global.ts
@@ -398,14 +398,21 @@ export const globalExtension = {
     seeres(resourceName: ResourceConstant): string {
         // 根据资源不同选择不同的查询目标
         const source = resourceName === RESOURCE_ENERGY ? STRUCTURE_STORAGE : STRUCTURE_TERMINAL
+        let total = 0
 
         let log = `${resourceName} 的分布如下：\n`
         // 遍历所有房间并检查对应的存储建筑
         log += Object.values(Game.rooms).map(room => {
-            if (room[source] && room[source].store[resourceName] > 0) return `${room.name} => ${room[source].store[resourceName]}`
+            // 统计数量
+            const amount = room[source].store[resourceName] || 0
+            total += amount
+
+            // 如果有就列出显示
+            if (room[source] && amount > 0) return `${room.name} => ${amount}`
             else return false
         }).filter(res => res).join('\n')
 
+        log += `共计: ${total}`
         return log
     },
 
--- src/mount.room.ts
+++ src/mount.room.ts
@@ -1460,18 +1460,18 @@ class PositionExtension extends RoomPosition {
      * 获取该位置周围的开采位空位
      */
     public getFreeSpace(): RoomPosition[] {
-        const terrain = Game.map.getRoomTerrain(this.roomName)
+        const terrain = new Room.Terrain(this.roomName)
         const result: RoomPosition[] = []
 
         const xs = [this.x - 1, this.x, this.x + 1]
         const ys = [this.y - 1, this.y, this.y + 1]
-        
+
         // 遍历 x 和 y 坐标
         xs.forEach(x => ys.forEach(y => {
             // 如果不是墙则 ++
             if (terrain.get(x, y) != TERRAIN_MASK_WALL) result.push(new RoomPosition(x, y, this.roomName))
         }))
-        
+
         return result
     }
 }
