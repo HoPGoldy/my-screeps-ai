aec6895cb365d08e46ea89f4aa0c9f0d1c23e0bf
hopgoldy <hpgless@outlook.com>
2020-07-28 01:23:51 +0800

    fix: assignPrototype 不会替换已有属性

--- src/utils.ts
+++ src/utils.ts
@@ -348,8 +348,6 @@ export const getName = {
  */
 export const assignPrototype = function(obj1: {[key: string]: any}, obj2: {[key: string]: any}) {
     Object.getOwnPropertyNames(obj2.prototype).forEach(key => {
-        if (key in obj1.prototype) return 
-
         if (key.includes('Getter')) {
             Object.defineProperty(obj1.prototype, key.split('Getter')[0], {
                 get: obj2.prototype[key],
