5645eee872e26a20b3bd7ed4f0d2d8bdeb75c45e
HoPGoldy <hpgless@outlook.com>
2021-03-25 10:53:20 +0800

    fix: 整理入口引入

---
+++ src/global.d.ts
@@ -0,0 +1,40 @@
+declare module NodeJS {
+    interface Global {
+        InterShardMemory: InterShardMemory
+        Game: Game
+        Memory: Memory
+        _: _.LoDashStatic
+    }
+}
+
+/**
+ * 包含任意键值对的对象
+ */
+type AnyObject = { [key: string]: any }
+
+/**
+ * 本项目中出现的颜色常量
+ */
+type Colors = 'green' | 'blue' | 'yellow' | 'red'
+
+/**
+ * 包含 store 属性的建筑
+ */
+type StructureWithStore =
+    StructureTower |
+    StructureStorage |
+    StructureContainer |
+    StructureExtension |
+    StructureFactory |
+    StructureSpawn |
+    StructurePowerSpawn |
+    StructureLink |
+    StructureTerminal |
+    StructureNuker
+
+/**
+ * 所有包含 id 字段的游戏对象
+ */
+interface ObjectWithId<T extends unknown = unknown> extends RoomObject {
+    id: Id<T>
+}
