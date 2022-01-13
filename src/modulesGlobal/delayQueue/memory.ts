756a966b5d8e17b8bbf989ced5fde50dd5026170
HoPGoldy <hpgless@outlook.com>
2022-01-13 14:44:12 +0800

    fix: 延迟队列保险处理

--- src/modulesGlobal/delayQueue/memory.ts
+++ src/modulesGlobal/delayQueue/memory.ts
@@ -5,10 +5,24 @@ const TASK_SPLIT_CODE = '▮'
 export const createMemoryAccessor = (getMemory: () => DelayQueueMemory) => ({
     queryTaskByTick (tick: number): DelayTask[] {
         const memory = getMemory()
-        const tickTaskStr = memory[tick]
-        if (!tickTaskStr) return []
+        const taskStrs: string[] = []
 
-        const taskStrs = tickTaskStr.split(TASK_SPLIT_CODE)
+        const allKeys = Object.keys(memory)
+        /**
+         * 这里做了个保险
+         * 正常情况下只需要读取对应 tick 的任务即可
+         * 但是考虑到如果服务器出现问题后可能会导致某些 tick 无法正常运行
+         * 所以这里实际上是“取出本 tick 及之前的任务进行处理”
+         *
+         * 由于这里的键都是递增的数字，所以可以认为 Object.keys 返回的键数组是有序的
+         */
+        for (const savedTick of allKeys) {
+            if (Number(savedTick) > tick) break
+            taskStrs.push(...memory[savedTick].split(TASK_SPLIT_CODE))
+        }
+
+        if (taskStrs.length <= 0) return []
+
         return taskStrs.map(str => JSON.parse(str))
     },
     deleteTaskByTick (tick: number) {
